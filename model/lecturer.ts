2df93aaeacc2945d78791a3881ba04d11c0505c6
Johan Pieck <johanpieck@gmail.com>
2022-05-16 12:49:29 +0200

    Initial version

---
+++ model/lecturer.ts
@@ -0,0 +1,108 @@
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import mapToLecturers from './lecturer-mapper';
+import { Lecturer } from '../types';
+import { connectionPool } from '../database';
+
+const getLecturers = async (onResult: (error: Error, lecturers: Lecturer[]) => void) => {
+    const query = `SELECT l.id AS lecturer_id, l.name AS lecturer_name, c.id AS course_id,  c.name AS course_name, c.description as course_description, c.phase as course_phase
+    FROM lecturer AS l
+  left join lecturer_course lc on l.id = lc.lecturer_id
+  left join course c on lc.course_id = c.id`;
+
+    /**
+     * You can avoid a try/catch block by wrapping the logic into an IIFE (immediately invoked function expression):
+     *  (async () => {
+     *      const rows = await connectionPool.query(query);
+     *      onResult(null, mapToLecturers(rows));
+     *  })().catch((err) => onResult(err));
+     */
+    try {
+        const [rows] = await connectionPool.query(query);
+        onResult(null, mapToLecturers(<RowDataPacket[]>rows));
+    } catch (error) {
+        onResult(error, null);
+    }
+};
+
+const getLecturer = async (
+    lecturerId: number,
+    onResult: (error: Error, lecturer: Lecturer) => void
+) => {
+    const query = `SELECT l.id AS lecturer_id, l.name AS lecturer_name, c.id AS course_id, c.name AS course_name, c.description AS course_description, c.phase AS course_phase
+  FROM lecturer AS l, course AS c, lecturer_course AS lc
+  WHERE l.id = ?
+  AND l.id = lc.lecturer_id
+  AND c.id = lc.course_id`;
+
+    try {
+        const [row] = await connectionPool.execute(query, [lecturerId]);
+        onResult(null, mapToLecturers(<RowDataPacket[]>row)[0]);
+    } catch (error) {
+        onResult(error, null);
+    }
+};
+
+const addLecturer = async (
+    lecturer: Lecturer,
+    onResult: (error: Error, addedLecturerId: number) => void
+) => {
+    const lecturerInsert = 'INSERT INTO lecturer (name) VALUES (?)';
+    const lecturerCourseInsert =
+        'INSERT INTO lecturer_course (lecturer_id, course_id) VALUES (?, ?)';
+
+    let connection;
+    try {
+        connection = await connectionPool.getConnection();
+        // Multiple queries are involved, so we execute them in a transaction to assure they will only get commited
+        // when all queries were succesful. Otherwise, all queries need to be rolled back.
+        await connection.beginTransaction();
+
+        const [result] = await connection.execute(lecturerInsert, [lecturer.name]);
+        const addedLecturerId = (<ResultSetHeader>result).insertId;
+
+        // we can't use forEach, since it expects a synchronous function and doesn't wait for promises
+        for (const course of lecturer.courses) {
+            await connection.execute(lecturerCourseInsert, [addedLecturerId, course.id]);
+        }
+
+        await connection.commit();
+        onResult(null, addedLecturerId);
+    } catch (error) {
+        if (connection) {
+            await connection.rollback();
+        }
+        onResult(error, null);
+    } finally {
+        if (connection) {
+            await connection.release();
+        }
+    }
+};
+
+const deleteLecturer = async (lecturerId: number, onResult: (error: Error) => void) => {
+    const lecturerDelete = 'DELETE FROM lecturer WHERE ID = ?';
+    const lecturerCourseDelete = 'DELETE FROM lecturer_course WHERE lecturer_id = ?';
+
+    let connection;
+
+    try {
+        connection = await connectionPool.getConnection();
+        await connection.beginTransaction();
+        await connection.execute(lecturerCourseDelete, [lecturerId]);
+        await connection.execute(lecturerDelete, [lecturerId]);
+
+        await connection.commit();
+        onResult(null);
+    } catch (error) {
+        if (connection) {
+            await connection.rollback();
+        }
+        onResult(error);
+    } finally {
+        if (connection) {
+            await connection.release();
+        }
+    }
+};
+
+export { getLecturers, getLecturer, addLecturer, deleteLecturer };
---
+++ routes/lecturer-router.ts
@@ -0,0 +1,163 @@
+/**
+ * @swagger
+ *   components:
+ *    schemas:
+ *      Lecturer:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: number
+ *            name:
+ *              type: string
+ *              description: Lecturer's name.
+ *            courses:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: number
+ *                  name:
+ *                    type: string
+ *                    description: Course name
+ *                  description:
+ *                    type: string
+ *                    description: Course description
+ *                  phase:
+ *                    type: number
+ *                    description: The phase within the education path
+ *      LecturerInput:
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *              description: Lecturer's name.
+ *            courses:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: number
+ */
+import express, { Request, Response, Handler } from 'express';
+import * as lecturerModel from '../model/lecturer';
+import { Lecturer } from '../types';
+
+const lecturerRouter = express.Router();
+
+/**
+ * @swagger
+ * /lecturers:
+ *   get:
+ *     summary: Get a list of lecturers and the courses they teach
+ *     responses:
+ *       200:
+ *         description: A list of lecturers.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Lecturer'
+ */
+lecturerRouter.get('/', (req: Request, res: Response) => {
+    lecturerModel.getLecturers((err: Error, lecturers: Lecturer[]) => {
+        if (err) {
+            res.status(500).json({ status: 'error', errorMessage: err.message });
+        } else {
+            res.status(200).json(lecturers);
+        }
+    });
+});
+
+/**
+ * @swagger
+ * /lecturers/{id}:
+ *   get:
+ *      summary: Get a lecturer by ID
+ *      responses:
+ *         200:
+ *           description: A lecturer
+ *           content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Lecturer'
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Lecturer ID
+ *          required: true
+ *          schema:
+ *            type: integer
+ *            format: int64
+ */
+lecturerRouter.get('/:id', (req: Request, res: Response) => {
+    const lecturerId = parseInt(req.params.id);
+    lecturerModel.getLecturer(lecturerId, (error: Error, lecturer: Lecturer) => {
+        if (error) {
+            res.status(500).json({ status: 'error', errorMessage: error.message });
+        } else {
+            res.status(200).json(lecturer);
+        }
+    });
+});
+
+/**
+ * @swagger
+ * /lecturers:
+ *   post:
+ *      summary: Add a lecturer
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LecturerInput'
+ *      responses:
+ *         200:
+ *            description: The ID of the new Lecturer
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: number
+ *                  description: Database ID
+ */
+lecturerRouter.post('/', (req: Request, res: Response) => {
+    const lecturer = <Lecturer>req.body;
+    lecturerModel.addLecturer(lecturer, (error: Error, lecturerId: number) => {
+        if (error) {
+            res.status(500).json({ status: 'error', errorMessage: error.message });
+        } else {
+            res.status(200).json({ status: 'success', lecturerId });
+        }
+    });
+});
+
+/**
+ * @swagger
+ * /lecturers/{id}:
+ *   delete:
+ *      summary: Delete a lecturer by ID
+ *      responses:
+ *         200:
+ *            description: Delete succesful
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Lecturer ID
+ *          required: true
+ *          schema:
+ *            type: integer
+ *            format: int64
+ */
+lecturerRouter.delete('/:id', (req: Request, res: Response) => {
+    const lecturerId = parseInt(req.params.id);
+    lecturerModel.deleteLecturer(lecturerId, (error: Error) => {
+        if (error) {
+            res.status(500).json({ status: 'error', errorMessage: error.message });
+        } else {
+            res.status(200).json({ status: 'success' });
+        }
+    });
+});
+
+export { lecturerRouter };
